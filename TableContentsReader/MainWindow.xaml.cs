fe159c1bc0fb4d322f0c007e0437d1da53ef4816
Jon Holmes <holmjona@isu.edu>
2019-01-11 15:28:39 -0700

    Code we did in demo.

--- TableContentsReader/MainWindow.xaml.cs
+++ TableContentsReader/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,38 @@ namespace TableContentsReader {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void BtnSayHello_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Hello"); MessageBox.Show("Hello Again");
+        }
+
+        private void  BooDooGoo (object s, RoutedEventArgs e) {
+            //MessageBox.Show("Other Button");
+            //MessageBox.Show("Other Button");
+
+            //var a = 5;
+            //int h;
+            //h = 5;
+
+            try {
+                MessageBox.Show("Attempt to read");
+                string[] chapters = File.ReadAllLines("tocc.txt");
+                MessageBox.Show("read successful");
+
+            } catch (Exception ex) {
+                MessageBox.Show("oops we had an error: " + ex.Message);
+
+            } finally {
+                // always fires
+                // close open files.
+                MessageBox.Show("In the finally");
+
+            }
+
+
+
+        }
+
+   
     }
 }
